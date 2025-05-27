0d94b5fb72336fefe894090289a03da63a86f3c5
Tiago Freitas <tiagohuehuehue@gmail.com>
2025-05-27 12:36:15 -0300

    Alterações na home page e no standard button

--- lib/components/standard_button.dart
+++ lib/components/standard_button.dart
@@ -2,58 +2,30 @@ import 'package:flutter/material.dart';
 
 class StandardButton extends StatelessWidget {
   final String text;
+  final VoidCallback onPressed;
 
-  const StandardButton({super.key, required this.text});
-
-  /*@override
-  State<StatefulWidget> createState() => _StandardButtonState();*/
+  const StandardButton({
+    super.key,
+    required this.text,
+    required this.onPressed,
+  });
 
   @override
   Widget build(BuildContext context) {
-    return TextButton(
-      onPressed: () {},
-      style: TextButton.styleFrom(
+    return ElevatedButton(
+      onPressed: onPressed,
+      style: ElevatedButton.styleFrom(
+        padding: EdgeInsets.symmetric(horizontal: 32, vertical: 12),
         fixedSize: Size(200, 50),
         shape: RoundedRectangleBorder(
-          borderRadius: BorderRadius.all(Radius.circular(5)),
-          side: BorderSide(
-            //color: Colors.black,
-            width: 2,
-          ),
+          borderRadius: BorderRadius.all(Radius.circular(12)),
+          side: BorderSide(width: 2),
         ),
       ),
       child: Text(
         text,
-        style: TextStyle(
-          fontFamily: "NewRocker",
-          //color: Colors.black,
-          fontSize: 30,
-        ),
+        style: TextStyle(fontFamily: "NewRocker", fontSize: 30),
       ),
     );
   }
 }
-
-/*class _StandardButtonState extends State<StatefulWidget> {
-  @override
-  Widget build(BuildContext context) {
-    return TextButton(
-      onPressed: () {},
-      style: TextButton.styleFrom(
-        shape: RoundedRectangleBorder(
-          borderRadius: BorderRadius.all(Radius.circular(5)),
-          side: BorderSide(color: Color.fromARGB(255, 0, 0, 0), width: 5),
-        ),
-      ),
-
-      child: Text(
-        widget.text,
-        style: TextStyle(
-          fontFamily: "NewRocker",
-          color: Colors.black,
-          fontSize: 30,
-        ),
-      ),
-    );
-  }
-}*/
--- lib/pages/home_page.dart
+++ lib/pages/home_page.dart
@@ -1,4 +1,6 @@
 import 'package:app_de_pedidos_de_hamburguer/components/standard_button.dart';
+import 'package:app_de_pedidos_de_hamburguer/pages/burguers_page.dart';
+import 'package:app_de_pedidos_de_hamburguer/pages/orders_page.dart';
 import 'package:flutter/material.dart';
 
 class HomePage extends StatefulWidget {
@@ -17,22 +19,46 @@ class _HomePageState extends State<HomePage> {
       body: Center(
         child: Column(
           mainAxisAlignment: MainAxisAlignment.center,
-          spacing: 5,
           children: [
-            Row(
-              mainAxisAlignment: MainAxisAlignment.center,
-              children: [
-                Image.asset("lib/images/burguer_logo.png", scale: 1.5),
-                Column(
-                  children: [
-                    Text("BURGUER", style: textTheme.titleLarge),
-                    Text("ORDERS", style: textTheme.titleLarge),
-                  ],
-                ),
-              ],
+            Spacer(flex: 2),
+            FittedBox(
+              child: Row(
+                mainAxisAlignment: MainAxisAlignment.center,
+                children: [
+                  Image.asset("lib/images/burguer_logo.png", scale: 1.75),
+                  Column(
+                    children: [
+                      Text("BURGUER\nORDERS", style: textTheme.titleLarge),
+                    ],
+                  ),
+                ],
+              ),
             ),
-            StandardButton(text: "Burguers"),
-            StandardButton(text: "Orders"),
+            Spacer(),
+            StandardButton(
+              text: "Burguers",
+              onPressed: () {
+                Navigator.push(
+                  context,
+                  MaterialPageRoute(
+                    builder: (BuildContext context) => BurguersPage(),
+                  ),
+                );
+              },
+            ),
+            SizedBox(height: 10),
+            StandardButton(
+              text: "Orders",
+              onPressed: () {
+                Navigator.push(
+                  context,
+                  MaterialPageRoute(
+                    builder: (BuildContext context) => OrdersPage(),
+                  ),
+                );
+              },
+            ),
+            Spacer(flex: 2),
           ],
         ),
       ),
